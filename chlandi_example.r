b2f8e256991105644b2e03258306c86feb506503
dmackinnon1 <dmackinnon1@gmail.com>
2016-06-15 21:20:28 -0400

    update chlandi example

--- chlandi_example.r
+++ chlandi_example.r
@@ -1,16 +1,27 @@
 # Generates an image similar to a "Chlandi Figure." Chlandi Figures are nodal patterns
 # that occur on vibrating surfaces.
+t <- 0:500
+f <- 2*pi/500
+i <- 0
+Chlandi  <- 0:500 * 0 # first row is zero, as sin(0) = 0
 
-t <- 0:100
-f <- 2*pi/100
-Chlandi  <- sin(1/2*f*t)*sin(1/2*f*0) + sin(f*t)*sin(f*0) + 2*sin(2*f*t)*sin(2*f*0)
-
-for ( i in 1:100) {
-  Chlandi  <- rbind(Chlandi , sin(1/2*f*t)*sin(1/2*f*i) + sin(f*t)*sin(f*i) + 2*sin(2*f*t)*sin(2*f*i) 
+# now add 500 more rows, first factor in each term varies as we go across a row (from t)
+# second factor of each term varies as we go down a column (from i)
+for ( i in 1:500) { 
+  Chlandi  <- rbind(Chlandi , # comment out different factors below to generate
+                              # a variety of images
+                    + sin(1/2*f*t)*sin(1/2*f*i) 
+                    + sin(f*t)*sin(f*i)  
+                    # + sin(3*f*t)*sin(3*f*i)  
+                    + sin(4*f*t)*sin(4*f*i)
+                    + sin(5*f*t)*sin(5*f*i)
+                    
   )
 }
 par(mfrow=c(1,1))
 
-# un comment out one of the below to display 
-#image(Chlandi , col=topo.colors(100))
-contour(Chlandi)
+Chlandi <- abs(Chlandi)
+
+# use the image or contour functions to display the results
+image(Chlandi, col=grey.colors(40, start=0, end=1))
+#contour(Chlandi)
