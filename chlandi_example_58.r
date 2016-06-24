f1a5028c918ee2daf6e0ae54f629ebe01af083c7
dmackinnon1 <dmackinnon1@gmail.com>
2016-06-24 17:03:39 -0400

    chlandi updates

--- chlandi_example_58.r
+++ chlandi_example_58.r
@@ -1,3 +1,4 @@
+# Chilandi 58 - similar to Chilandi's original figure 58
 
 # Generates an image similar to a "Chlandi Figure." Chlandi Figures are nodal patterns
 # that occur on vibrating surfaces.
@@ -5,32 +6,23 @@
 # The surface is modeled as a matrix, each point representing the 
 # displacement of the surface while vibrating.
 # The vibration is modled as the sum of a set of terms, each of which
-# is a product of sin waves - one along the x direction, one along the y direction.
-# The sin wave terms have different frequencies - starting with the 
+# is a product of cos waves - one along the x direction, one along the y direction.
+# The cos wave terms have different frequencies - starting with the 
 # 'fundamental' frequency (wavelength is 1/2 the length of the surface),
 # and followed by various 'harmonics'. 
 
 t <- 0:500
 f <- 2*pi/500
 i <- 0
-Chlandi  <- 0:500 * 0 # first row is zero, as sin(0) = 0
+Chlandi  <- rep(1, 501) # first row is one, as cos(0) = 1
 
 # Now add 500 more rows, first factor in each term varies as we go across a row (from t)
 # second factor of each term varies as we go down a column (from i)
 for ( i in 1:500) { 
-  Chlandi  <- rbind(Chlandi , # comment out different factors below to generate
-                              # a variety of images
-                    +  sin(1/2*f*t)*sin(1/2*f*i) 
-                    #+ sin(f*t)*sin(f*i)  
-                    #+ sin(3/2*f*t)*sin(3/2*f*i)  
-                    #+ sin(2*f*t)*sin(2*f*i)
-                    + sin(5/2*f*t)*sin(5/2*f*i)
-                    #+ sin(3*f*t)*sin(3*f*i)
-                    #- sin(7/2*f*t)*sin(7/2*f*i)
-                    #+ sin(4*f*t)*sin(4*f*i)
-                    # sin(9/2*f*t)*sin(9/2*f*i)
-                    
-  )
+  Chlandi  <- rbind(Chlandi , 
+                    + cos(1/2*f*t)*cos(1/2*f*i) 
+                    + cos(5/2*f*t)*cos(5/2*f*i)
+                   )
 }
 # Reset the graph layout in case it was previously set elsewhere.
 par(mfrow=c(1,1))
